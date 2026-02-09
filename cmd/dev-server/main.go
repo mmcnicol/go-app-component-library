dff5f344689a5c42d3d7d0e41dd0e1339644f05d
M McNicol <matthew.mcnicol@gmail.com>
2026-02-09 03:12:52 +0000

    WIP

--- cmd/dev-server/main.go
+++ cmd/dev-server/main.go
@@ -3,7 +3,6 @@ package main
 
 import (
     "bytes"
-    "embed"
     "encoding/json"
     "flag"
     "fmt"
@@ -12,7 +11,6 @@ import (
     "os"
     "os/exec"
     "path/filepath"
-    "runtime"
     "strings"
     "sync"
     "time"
