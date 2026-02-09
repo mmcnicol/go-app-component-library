69b4fe82f4db56a1bd893562030cf27e25fe9a73
M McNicol <matthew.mcnicol@gmail.com>
2026-02-09 22:12:51 +0000

    WIP

--- pkg/components/select_one/select_one_stories.go
+++ pkg/components/select_one/select_one_stories.go
@@ -10,6 +10,7 @@ import (
 // Use init() to auto-register when this package is imported
 func init() {
 
+    /*
     selectOneDefault := &SelectOne{
         Label: "Label text.",
         Options: []string{"Go", "Python", "Rust", "JavaScript"},
@@ -28,5 +29,27 @@ func init() {
     storybook.Register("Select One", "ReadOnly", func() app.UI {
         return selectOneReadOnly
     })
+    */
+
+    selectOptions := []string{"Go", "Python", "Rust", "JavaScript"}
+
+    storybook.Register("Form", "Select One", 
+		map[string]*storybook.Control{
+			"Label": {Label: "Label", Type: storybook.ControlText, Value: "Label Text."},
+			"Disabled": {Label: "Disabled", Type: storybook.ControlBool, Value: false},
+			"Options":    {Label: "Options", Type: storybook.ControlText, Value: selectOptions}, 
+		},
+		func(controls map[string]*storybook.Control) app.UI {
+			labelString := controls["Label"].Value.(string)
+			isDisabled := controls["Disabled"].Value.(bool)
+			opts := controls["Options"].Value.([]string)
+
+			return &SelectOne{
+				Label: labelString,
+				Disabled: isDisabled,
+				Options: opts,
+			}
+		},
+	)
     
 }
