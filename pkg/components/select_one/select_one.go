9acb8cc46b7bf3ea7a16d85a633bcef1844c287e
M McNicol <matthew.mcnicol@gmail.com>
2026-02-09 22:29:36 +0000

    WIP

--- pkg/components/select_one/select_one.go
+++ pkg/components/select_one/select_one.go
@@ -40,9 +40,9 @@ func (o *SelectOne) Render() app.UI {
 			Class(containerClass).
 			Disabled(o.Disabled).
 			//SelectedValue(o.SelectedValue). // Keeps the UI in sync with Go state
-			//OnChange(o.onSelectChange).
+			OnChange(o.onSelectChange).
 			//OnChange(o.ValueTo(&o.SelectedValue)).
-			OnSelect(o.ValueTo(&o.SelectedValue)).
+			//OnSelect(o.ValueTo(&o.SelectedValue)).
 			Body(
 				app.Option().
 					Disabled(true).
