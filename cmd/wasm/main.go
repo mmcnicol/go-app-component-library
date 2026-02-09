67063b59c4c48c04f0696ab1f2316fa088711ff7
M McNicol <matthew.mcnicol@gmail.com>
2026-02-09 01:45:30 +0000

    WIP

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
-	"github.com/mmcnicol/go-app-component-library/pkg/components"
+	"go-app-component-library/pkg/components"
 )
 
 func main() {
