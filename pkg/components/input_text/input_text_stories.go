1d0bf80ec6af56a643645e00a34fc74e1b0cde92
M McNicol <matthew.mcnicol@gmail.com>
2026-02-09 21:42:02 +0000

    WIP

--- pkg/components/input_text/input_text_stories.go
+++ pkg/components/input_text/input_text_stories.go
@@ -42,14 +42,14 @@ func init() {
 	storybook.Register("Built In", "Input Text", 
 		map[string]*storybook.Control{
 			"Label": {Label: "Label Text", Type: storybook.ControlText, Value: "Label Text."},
-			"Disabled":   {Label: "Disabled", Type: storybook.ControlBool, Value: false},
-			"Value":    {Label: "Options", Type: storybook.ControlText, Value: ""}, 
-			"Placeholder":    {Label: "Placeholder", Type: storybook.ControlText, Value: "First Name"}, 
+			"Value": {Label: "Value", Type: storybook.ControlText, Value: ""}, 
+			"Disabled": {Label: "Disabled", Type: storybook.ControlBool, Value: false},
+			"Placeholder": {Label: "Placeholder", Type: storybook.ControlText, Value: "First Name"}, 
 		},
 		func(controls map[string]*storybook.Control) app.UI {
 			labelText := controls["Label Text"].Value.(string)
-			isDisabled := controls["Disabled"].Value.(bool)
 			valueString := controls["Value"].Value.(string)
+			isDisabled := controls["Disabled"].Value.(bool)
 			placeholderString := controls["Placeholder"].Value.(string)
 
 			return &InputText{
