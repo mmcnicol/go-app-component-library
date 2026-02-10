509b0b747f0fb07b3aa380478fec7b64cb557ab8
M McNicol <matthew.mcnicol@gmail.com>
2026-02-10 23:57:54 +0000

    WIP

--- pkg/components/icon/icon.go
+++ pkg/components/icon/icon.go
@@ -45,7 +45,9 @@ func (i *Icon) GetIcon(name string, size int) app.UI {
 	case "hospital-inpatient":
 		d = "M18 10V7c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v3c-1.1 0-2 .9-2 2v5h1.33L6 19h1l.67-2h8.67l.66 2h1l.67-2H20v-5c0-1.1-.9-2-2-2zm-5 2h-2v-2h2v2zm5 2H6v-2h12v2z"
 	case "hospital-outpatient":
-		d = "M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 11h-4v4h-4v-4H6v-4h4V6h4v4h4v4z"
+		//d = "M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 11h-4v4h-4v-4H6v-4h4V6h4v4h4v4z"
+	    // Person (circle for head, line for body) with arrow pointing to building
+    	d = "M9 6c0-1.66 1.34-3 3-3s3 1.34 3 3-1.34 3-3 3-3-1.34-3-3zm3 1c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm-2 7h4v2h-4v-2zm9-2h-2v2h2v-2zm-1 4h-1v1h1v-1zm-6 6h6v2H11v-2zm6-8h-1V7h1v1zm-2 0h-1V7h1v1zm-2 8h2v1h-2v-1zm6-4h-1v1h1v-1zm-8-4h1v1h-1V9zm6 0h-1v1h1V9zm-1 5l3-3 1.5 1.5L17.83 14H15v2h2.83l-1.33 1.33L16.5 18.5 13.5 15.5z"
 	case "hospital-alert-covid":
 		d = "M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13h-2v2h-2v-2h-2v-2h2v-2h2v2h2v2zm-4.88-6.42L11 8.5V6h2v2.5l-.88.08zM7 13v-2h2.5l.08.88.08.12H7zm5 6v-2.5l.88-.08.12-.08V19h-2zm5-6h-2.5l-.08-.88-.08-.12H17v2z"
 	case "spinner":
