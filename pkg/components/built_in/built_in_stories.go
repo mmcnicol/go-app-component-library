e120851c6c95e979f0757cf29bc6f4b6e4ad2014
M McNicol <matthew.mcnicol@gmail.com>
2026-02-11 15:22:24 +0000

    WIP

--- pkg/components/built_in/built_in_stories.go
+++ pkg/components/built_in/built_in_stories.go
@@ -42,52 +42,6 @@ func init() {
 
 	selectOptions := []string{"Go", "Python", "Rust", "JavaScript"}
 
-	/*
-	storybook.Register("Built In", "Select", func() app.UI {
-		return app.Select().
-			Body(
-				app.Option().
-					Disabled(true).
-					Text("Choose an option..."),
-				app.Range(selectOptions).Slice(func(i int) app.UI {
-					opt := selectOptions[i]
-					return app.Option().
-						Value(opt).
-						Text(opt)
-				}),
-			)
-	})
-	*/
-
-	/*
-	storybook.Register("Built In", "Select", 
-		map[string]*storybook.Control{
-			"PromptText": {Label: "Prompt Text", Type: storybook.ControlText, Value: "Choose an option..."},
-			"Disabled": {Label: "Disabled", Type: storybook.ControlBool, Value: false},
-			"Options": {Label: "Options", Type: storybook.ControlText, Options: selectOptions},
-		},
-		func(controls map[string]*storybook.Control) app.UI {
-			// Retrieve values from the controls map
-			promptText := controls["PromptText"].Value.(string)
-			isDisabled := controls["Disabled"].Value.(bool)
-			selectOptions := controls["Options"].Value.([]string)
-
-			return app.Select().
-			Body(
-				app.Option().
-					Disabled(isDisabled).
-					Text(promptText),
-				app.Range(selectOptions).Slice(func(i int) app.UI {
-					opt := selectOptions[i]
-					return app.Option().
-						Value(opt).
-						Text(opt)
-				}),
-			)
-		},
-	)
-	*/
-
 	storybook.Register("Built In", "Select", 
 		map[string]*storybook.Control{
 			"PromptText": {Label: "Prompt Text", Type: storybook.ControlText, Value: "Choose an option..."},
