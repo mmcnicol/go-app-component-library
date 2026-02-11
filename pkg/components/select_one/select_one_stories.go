0a768a28c2e09e7393a360240fe359c8f4d5f590
M McNicol <matthew.mcnicol@gmail.com>
2026-02-11 15:55:47 +0000

    WIP

--- pkg/components/select_one/select_one_stories.go
+++ pkg/components/select_one/select_one_stories.go
@@ -47,7 +47,7 @@ func init() {
 		func(controls map[string]*storybook.Control) app.UI {
 			promptText := controls["PromptText"].Value.(string)
 			isDisabled := controls["Disabled"].Value.(bool)
-			opts := controls["Options"].Value.([]string)
+			opts := controls["Options"].Options.([]string)
             selectedValue := controls["SelectedValue"].Value.(string)
 
 			return &SelectOne{
