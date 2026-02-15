195d02eb3b52412ebda16d067c3c1c6173b38f0b
M McNicol <matthew.mcnicol@gmail.com>
2026-02-15 22:57:32 +0000

    WIP

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -21,8 +21,6 @@ import (
 	_ "github.com/mmcnicol/go-app-component-library/pkg/components/label"
 	_ "github.com/mmcnicol/go-app-component-library/pkg/components/table"
 	_ "github.com/mmcnicol/go-app-component-library/pkg/components/panel"
-	_ "github.com/mmcnicol/go-app-component-library/pkg/components/chart"
-	_ "github.com/mmcnicol/go-app-component-library/pkg/components/viz"
 
 	"log"
 	"net/http"
